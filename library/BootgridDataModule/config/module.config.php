ba5bd0c29f58a859fa25fa6148789e476f6b66b2
stephan <stephan@debian>
2015-02-02 16:00:11 +0100

    bootgrid controller added

--- library/BootgridDataModule/config/module.config.php
+++ library/BootgridDataModule/config/module.config.php
@@ -14,12 +14,24 @@ return [
         				'action' => 'data-provider',
         			]
         		]
+       		],
+        	'bootgrid' => [
+        		'type' => 'Segment',
+        		'options' => [
+        			'route' => '/bootgrid[/:entity[/:action]]',
+        			'defaults' => [
+        				'__NAMESPACE__' => 'BootgridDataModule\Controller',
+        				'controller' => 'Bootgrid',
+        				'action' => 'show'
+        			]
+        		]
        		]
         ]
     ],
     'controllers' => [
         'invokables' => [
-        	'BootgridDataModule\Controller\BootgridData' => 'BootgridDataModule\Controller\BootgridDataController'
+        	'BootgridDataModule\Controller\BootgridData' => 'BootgridDataModule\Controller\BootgridDataController',
+        	'BootgridDataModule\Controller\Bootgrid' => 'BootgridDataModule\Controller\BootgridController'
         ],
     ],
     'view_manager' => [
---
+++ library/BootgridDataModule/src/BootgridDataModule/Controller/BootgridController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace BootgridDataModule\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+class BootgridController extends AbstractActionController
+{
+	function showAction() {
+		 
+	}
+	
+    function createAction() {
+    	
+    }
+	
+    function editAction() {
+    	 
+    }
+	
+    function deleteAction() {
+    	 
+    }
+}
